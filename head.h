93b3d42a36395b62941f07311de23df59126f39b
RyanWangllng <wangyouzeng1998@163.com>
2021-09-23 20:42:07 +0800

    头文件

---
+++ head.h
@@ -0,0 +1,16 @@
+#ifndef _HEAD_
+#define _HEAD_
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <iostream>
+
+#endif
