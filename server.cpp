78838afe6bc266a8e33d1b7d7d7c36dbb21ae8e0
RyanWangllng <wangyouzeng1998@163.com>
2021-09-24 14:35:49 +0800

    多线程服务器实现

--- server.cpp
+++ server.cpp
@@ -1,15 +1,27 @@
-#include "head.h"
-using namespace std;
+// #include "head.h"
+#include "server.h"
 
-int main() {
-    // 创建服务器套接字，是一个文件描述符
-    int server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+// 构造函数
+server::server(int port, string ip) : server_port(port), server_ip(ip) {}
 
-    // 定义服务器socket地址结构体
+// 析构函数
+server::~server() {
+    for (auto connection : sock_arr) {
+        close(connection);
+    }
+    close(server_sockfd);
+}
+
+// 服务器运行
+void server::run() {
+    // 服务器套接字
+    server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // 服务器的socket地址结构体
     struct sockaddr_in server_sockaddr;
     server_sockaddr.sin_family = AF_INET;                       // TCP/IPv4协议族
-    server_sockaddr.sin_port = htons(8023);                     // 转换端口：主机字节序 -> 网络字节序
-    server_sockaddr.sin_addr.s_addr = inet_addr("127.0.0.1");   // ip地址，环回地址，相当于本机ip
+    server_sockaddr.sin_port = htons(server_port);                     // 转换端口：主机字节序 -> 网络字节序
+    server_sockaddr.sin_addr.s_addr = inet_addr(server_ip.c_str());   // ip地址，环回地址，相当于本机ip
 
     // bind将套接字描述符和本地ip地址及端口号绑定
     if (bind(server_sockfd, (struct sockaddr*)&server_sockaddr, sizeof(server_sockaddr)) == -1) {
@@ -27,30 +39,35 @@ int main() {
     struct sockaddr_in client_addr;
     socklen_t length = sizeof(client_addr);
 
-    // 服务器接受客户端连接，connect成功后是用于通信的文件描述符
-    int connect = accept(server_sockfd, (struct sockaddr*)&client_addr, &length);
-    if (connect < 0) {
-        perror("connect error");
-        exit(1);
-    }
-    cout << "客户端成功连接" << endl;
-
-    // 接受缓冲区
-    char buffer[1000];
-
-    // 不断接受客户端发来的数据
+    // 不断与客户端创建新的连接，并创建子线程为其服务
     while (1) {
-        // 清空缓冲区
-        memset(buffer, 0, sizeof(buffer));
-        // 接收数据，返回接受到数据的长度
-        int len = recv(connect, buffer, sizeof(buffer), 0);
-        // 客户端发送exit或异常结束时，退出
-        if (strcmp(buffer, "exit") == 0 || len <= 0) break;
-        cout << "收到客户端信息：" << buffer << endl;
+        int connection = accept(server_sockfd, (struct sockaddr*)&client_addr, &length);
+        if (connection < 0) {
+            perror("connect error");
+            exit(1);
+        }
+        cout << "套接字描述符为 " << connection << " 的客户端成功连接！" << endl;
+        sock_arr.push_back(connection);
+        // 创建子线程
+        thread t(server::RecvMsg, connection);
+        // 设置线程分离状态，用join会导致主线程阻塞
+        t.detach();
     }
+}
 
-    // 关闭文件描述符
-    close(connect);
-    close(server_sockfd);
-    return 0;
+// 子线程工作的静态函数
+// 前面加static会报错
+void server::RecvMsg(int connection) {
+    // 接收缓冲区
+    char buffer[1000];
+    // 不断接收数据
+    while (1) {
+        memset(buffer, 0, sizeof(buffer));
+        int len = recv(connection, buffer, sizeof(buffer), 0);
+        // 客户端发送exit或异常结束时，退出
+        if (strcmp(buffer, "exit") == 0 || len <= 0) {
+            break;
+        }
+        cout << "收到套接字描述符为 " << connection << " 的客户端发来的信息： " << buffer << endl;
+    }
 }
