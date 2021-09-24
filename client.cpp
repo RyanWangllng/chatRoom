2e486412657e6373ea8779de3c38dcd874f0f180
RyanWangllng <wangyouzeng1998@163.com>
2021-09-24 15:47:20 +0800

    多线程客户端定义

--- client.cpp
+++ client.cpp
@@ -1,35 +1,55 @@
-#include "head.h"
-using namespace std;
+#include "client.h"
 
-int main() {
-    // 创建客户端套接字，也是一个文件描述符
-    int sock_client = socket(AF_INET, SOCK_STREAM, 0);
+client::client(int port, string ip) : server_port(port), server_ip(ip) {}
+client::~client() {
+    close(sock_connection);
+}
+
+void client::run() {
+    sock_connection = socket(AF_INET, SOCK_STREAM, 0);
 
     // 定义服务器socket地址结构体
-    struct sockaddr_in seraddr;
+    struct sockaddr_in servaddr;
     // memset(&seraddr, 0, sizeof(seraddr));
-    seraddr.sin_family = AF_INET;                       // TCP/IPv4协议族
-    seraddr.sin_port = htons(8023);                     // 转换端口：主机字节序 -> 网络字节序
-    seraddr.sin_addr.s_addr = inet_addr("127.0.0.1");   // ip地址，环回地址，相当于本机ip
+    servaddr.sin_family = AF_INET;                               // TCP/IPv4协议族
+    servaddr.sin_port = htons(server_port);                      // 转换端口：主机字节序 -> 网络字节序
+    servaddr.sin_addr.s_addr = inet_addr(server_ip.c_str());     // ip地址，环回地址，相当于本机ip
 
     // 客户端请求连接服务器
-    if (connect(sock_client, (struct sockaddr*)&seraddr, sizeof(seraddr)) < 0) {
+    if (connect(sock_connection, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
         perror("connect error");
         exit(1);
     }
     cout << "连接服务器成功！" << endl;
 
+    // 创建发送线程和接收线程
+    thread send_t(client::SendMsg, sock_connection), recv_t(client::RecvMsg, sock_connection);
+    send_t.join();
+    cout << "发送线程已关闭！" << endl;
+    recv_t.join();
+    cout << "接收线程已关闭！" << endl;
+    return;
+}
+
+void client::SendMsg(int connection) {
     char sendbuf[100];
-    char recvbuf[100];
     while (1) {
         memset(sendbuf, 0, sizeof(sendbuf));
         cin >> sendbuf;
-        // 在TCP连接上发送数据
-        send(sock_client, sendbuf, strlen(sendbuf), 0);
-        if (strcmp(sendbuf, "exit") == 0) break;
+        // 发送数据
+        int ret = send(connection, sendbuf, strlen(sendbuf), 0);
+        if (strcmp(sendbuf, "exit") == 0 || ret <= 0) break;
     }
+}
 
-    // 关闭文件描述符
-    close(sock_client);
-    return 0;
+void client::RecvMsg(int connection) {
+    // 接受缓冲区
+    char recvbuf[1000];
+    // 不断接收数据
+    while (1) {
+        memset(recvbuf, 0, sizeof(recvbuf));
+        int len = recv(connection, recvbuf, sizeof(recvbuf), 0);
+        if (len <= 0) break;
+        cout << "收到服务器发来的消息： " << recvbuf << endl;
+    }
 }
